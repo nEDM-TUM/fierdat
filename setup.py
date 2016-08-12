80f23e3e3bf2c53483497089cbe8fa082c8a1bd0
Matthew Bales <matthew.bales@gmail.com>
2016-08-12 14:53:31 +0200

    Basic directory and file structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fierdat',
+      version='0.1',
+      description='Fierlinger Group Data I/O and Analysis Python Library',
+      url='https://github.com/nEDM-TUM/fierdat',
+      author='Fierlinger Group',
+      author_email='matthew.bales@tum.de',
+      license='GPL',
+      packages=['fierdat'],
+      zip_safe=False)
